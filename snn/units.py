076ee177099d318bd024f361d58942dc4c34d563
psj8252 <psj8252@naver.com>
2021-07-18 17:49:07 +0900

    feat: Implement Neurons class

--- snn/units.py
+++ snn/units.py
@@ -1,39 +1,27 @@
-from typing import Tuple
+from typing import List, Tuple
+
+from .configs import NeuronConfig
 
 
 class Neuron:
-    def __init__(
-        self,
-        rest_potential: float,
-        min_potential: float,
-        threshold_potential: float,
-        excitatory_potential: float,
-        leakage_factor: float,
-        rest_period: int,
-    ):
+    def __init__(self, cfg: NeuronConfig):
         """
         Initialize neuron
 
-        :param rest_potential: potential when resting
-        :param min_potential: minimum potential
-        :param threshold_potential: threshold that fire a spike when potential is over than
-        :param excitatory_potential: added potential when excited
-        :param leakage_factor: decreasing potential by leakage_factor
-        :param rest_period: the number of time step in which neuron can't be receive input potential
+        :param cfg: neuron config instance
         """
-        self.rest_potential = rest_potential
-        self.min_potential = min_potential
-        self.threshold_potential = threshold_potential
-        self.excitatory_potential = excitatory_potential
-        self.leakage_factor = leakage_factor
-        self.rest_period = rest_period
+        self.rest_potential = cfg.rest_potential
+        self.min_potential = cfg.min_potential
+        self.threshold_potential = cfg.threshold_potential
+        self.excitatory_potential = cfg.excitatory_potential
+        self.leakage_factor = cfg.leakage_factor
+        self.rest_period = cfg.rest_period
 
-        self.potential = rest_potential
-        self.rest = 0
+        self.reset()
 
     def __call__(self, input_potential: float) -> Tuple[bool, float]:
         """
-        Input spike to this neuron or not
+        Input potentials to this neuron
 
         :param input_potential: input potential
         :returns: tuple of `is firing` and `potential of neuron`
@@ -53,3 +41,40 @@ class Neuron:
             self.potential += self.excitatory_potential
             self.rest += self.rest_period
         return fire_spike, self.potential
+
+    def reset(self):
+        """Reset neuron's status"""
+        self.rest = 0
+        self.potential = self.rest_potential
+
+
+class Neurons:
+    """A bunch of Neurons of same parameters"""
+
+    def __init__(self, num_neurons: int, cfg: NeuronConfig):
+        self.neurons = [Neuron(cfg) for _ in range(num_neurons)]
+
+    def __call__(self, input_potentials: List[float]) -> Tuple[List[bool], List[float]]:
+        """
+        Input potentials to each neuron and return spikes and potentials
+
+        :param input_potentials: input potentials for each neuron
+        :returns: tuple of `is firing` list and `potential` list of each neuron
+        """
+        assert len(input_potentials) == len(
+            self.neurons
+        ), "The number of potential is different from the number of neurons!"
+
+        fire_spikes = []
+        output_potentials = []
+        for input_potential, neuron in zip(input_potentials, self.neurons):
+            fire_spike, output_potential = neuron(input_potential)
+            fire_spikes.append(fire_spike)
+            output_potentials.append(output_potential)
+
+        return fire_spikes, output_potentials
+
+    def reset(self):
+        """Reset neuron's status"""
+        for neuron in self.neurons:
+            neuron.reset()
