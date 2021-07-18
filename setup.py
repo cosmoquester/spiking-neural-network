db28406911d3393ec3b23fda5c80f37343ea9aec
psj8252 <psj8252@naver.com>
2021-07-18 17:48:47 +0900

    build: Add numpy to package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.0.1",
     description="Practice for spiking neural network (SNN)",
     python_requires=">=3.7",
-    install_requires=[],
+    install_requires=["numpy"],
     url="https://github.com/cosmoquester/spiking-neural-network.git",
     author="Park Sangjun",
     packages=find_packages(exclude=["tests"]),
